6c12d81f4b7aa5694a5e052726d2fb9339a81925
Dercio Anselmo <dercio@branchdev.io>
2026-05-27 00:16:39 +0400

    Navbar

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,22 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "GapScan" },
+    { name: "description", content: "Smart feedback for your dram job " },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className={"bg-[url('/images/bg-main.svg')] bg-cover"}>
+    <Navbar />
+    <section>
+      <div className="main-section">
+        <h1>Track Your Application & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback</h2>
+      </div>
+    </section>
+
+  </main>
 }
