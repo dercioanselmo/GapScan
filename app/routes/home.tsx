7b40adda1261a2333bd298e5f7648a87e970c374
Dercio Anselmo <dercio@branchdev.io>
2026-05-27 10:58:52 +0400

    Resume Card, Resume Score Circle

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,12 +13,21 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return <main className={"bg-[url('/images/bg-main.svg')] bg-cover"}>
     <Navbar />
-    <section>
-      <div className="main-section">
+    <section className="main-section">
+      <div className="page-heading py-16">
         <h1>Track Your Application & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback</h2>
       </div>
     </section>
 
+    {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+              <div>
+                <ResumeCard key={resume.id} resume={resume} />
+              </div>
+          ))}
+        </div>
+    )}
   </main>
 }
