c46640ca8c00d251c020388c68e491e042646d45
Dercio Anselmo <dercio@branchdev.io>
2026-05-27 15:21:53 +0400

    Authentication feature

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,4 +1,6 @@
 import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
+import {useEffect} from "react";
 
 
 export const meta = () =>([
@@ -8,6 +10,13 @@ export const meta = () =>([
 
 const Auth = () => {
     const { isLoading, auth } = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(auth.isAuthenticated) navigate(next);
+    }, [auth.isAuthenticated, next])
     return (
         <main className="bg-[url('images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
             <div className="gradient-border shadow-lg">
