cf362af38b7575a195cf53b29544647c6a82bf89
Dercio Anselmo <dercio@branchdev.io>
2026-05-27 13:19:37 +0400

    Auth initial implementation Puter setup

--- app/root.tsx
+++ app/root.tsx
@@ -33,9 +33,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <script src="https://js.puter.com/v2/"></script>
+
         {children}
-        <ScrollRestoration />
-        <Scripts />
+          <ScrollRestoration />
+          <Scripts />
       </body>
     </html>
   );
