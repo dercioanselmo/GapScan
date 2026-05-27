c188d7081ff264f8104dc7d97adb215c4e915926
Dercio Anselmo <dercio@branchdev.io>
2026-05-27 22:29:57 +0400

    After upload file, convert PDF to Image to show in the UI, and assemble the Analyze logic

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,8 +1,12 @@
 import {useState} from 'react'
 import Navbar from "~/components/Navbar";
 import FileUploader from "~/components/FileUploader";
+import {usePuterStore} from "~/lib/puter";
+import {useNavigate} from "react-router"
 
 const Upload = () => {
+    const {auth, isLoading, fs, ai, kv } = usePuterStore();
+    const navigate = useNavigate();
     const [isProcessing, setIsProcessing] = useState(false);
     const [statusText, setStatusText] = useState("");
     const [file, setFile] = useState<File | null>(null);
@@ -11,19 +15,47 @@ const Upload = () => {
         setFile(file);
     }
 
+    const handleAnalyze = async ({companyName, jobTitle, jobDescription, file }: {companyName: string, jobTitle: string, jobDescription: string, file: File}) => {
+        setIsProcessing(true);
+        setStatusText("Uploading the file . . .")
+
+        const uploadFile = await fs.upload([file]);
+        if (!uploadFile) return setStatusText("Error: Failed to upload file");
+
+        setStatusText("Converting to image . . .");
+        const imageFile = await convertPdfToImage(file);
+        if (!imageFile.file) return setStatusText("Error: Failed convert PDF to image");
+
+        setStatusText("Uploading the image . . .")
+        const uploadedImage = await fs.upload([imageFile.file]);
+        if (!uploadedImage) return setStatusText("Error: Failed to upload image");
+
+        setStatusText("Preparing data . . .")
+        const uuid = generateUUID();
+        const data = {
+            id: uuid,
+            resumePath: uploadedFile.path,
+            imagePath: uploadedImage.path,
+            companyName, jobTitle, jobDescription,
+            feedback: ''
+        }
+        await kv.set(`resume:${ uuid }`, JSON.stringify(data));
+        setStatusText("Analyzing . . .")
+
+    }
+
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const form = e.currentTarget.closest('form');
         if(!form) return;
         const formData = new FormData(form);
 
-        const companyName = formData.get('company-name');
-        const jobTitle = formData.get('job-title');
-        const jobDescription = formData.get('job-description');
+        const companyName = formData.get('company-name') as string;
+        const jobTitle = formData.get('job-title') as string;
+        const jobDescription = formData.get('job-description') as string;
 
-        console.log({
-            companyName,jobTitle, jobDescription, file
-        })
+        if(!file) return;
+        handleAnalyze({companyName, jobTitle, jobDescription, file});
     }
 
     return (
