fbf290d1e83b2858886c4bf3ac674cbf35954a3d
Dercio Anselmo <dercio@branchdev.io>
2026-05-28 01:17:10 +0400

    Resume Analysis result - initial page implementation

---
+++ app/routes/resume.tsx
@@ -0,0 +1,65 @@
+import {useParams} from "react-router";
+import {useNavigate} from "react-router";
+import {useState} from 'react'
+
+export const meta = () =>([
+    {title: 'GapScan | Review'},
+    {name: 'description', content: 'Detailed overview of your resume'},
+])
+
+
+const Resume = () => {
+    const { id } = useParams();
+    const { auth, isLoading, fs, kv } = usePuterStore();
+    const[imageUrl, setImageUrl] = useState('');
+    const[resumeUrl, setResumeUrl] = useState('');
+    const[feedback, setFeedback] = useState('');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const loadResume = async () => {
+            const resume = await kv.get(`/resume/${id}`);
+
+            if (!resume) return;
+
+            const data = JSON.parse(resume.data);
+
+            const resumeBlob = await fs.read(data.resumePath);
+            if (!resumeBlob) return;
+
+            const pdfBlob = new Blob([resumeBlob], { type: 'application/pdf' });
+            const resumeUrl = URL.createObjectURL(pdfBlob);
+            setResumeUrl(resumeUrl);
+
+            const imageBlob = await fs.read(data.imagePath);
+            if(!imageBlob) return;
+            const imageUrl = URL.createObjectURL(imageBlob);
+            setImageUrl(imageUrl);
+
+            setFeedback(data.feedback);
+        }
+
+        loadResume();
+    }, []);
+    return (
+        <main className="!pt-0">
+            <nav className="resume-nav">
+                <Link to="/" className="back-button">
+                    <img src="/icons/back.png" alt="logo" className="w-2.5 h-2.5" />
+                    <span className="text-gray-800 text-sm font-semibold">Back to Homepage</span>
+                </Link>
+            </nav>
+            <div className="flex flex-row w-full max-lg:flex-col-reverse">
+                <section className="feedback-section">
+                    {imageUrl && resumeUrl && (
+                        <div className=" animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
+
+                        </div>
+                    )}
+                </section>
+
+            </div>
+        </main>
+    )
+}
+export default Resume
