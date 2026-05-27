ba9b65a026c2b7b47043cf5f8777361c76825114
Dercio Anselmo <dercio@branchdev.io>
2026-05-27 20:43:24 +0400

    File uploader component

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,9 +1,16 @@
 import {useState} from 'react'
 import Navbar from "~/components/Navbar";
+import FileUploader from "~/components/FileUploader";
 
 const Upload = () => {
     const [isProcessing, setIsProcessing] = useState(false);
     const [statusText, setStatusText] = useState("");
+    const [file, setFile] = useState<File | null>(null);
+
+    const handleFileSelect = (file: File | null) => {
+        setFile(file);
+    }
+
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {}
 
     return (
@@ -38,7 +45,7 @@ const Upload = () => {
                         </div>
                         <div className="form-div">
                             <label htmlFor="uploader">Upload Resume</label>
-                            <div>Uploader</div>
+                            <FileUploader onFileSelect={handleFileSelect}/>
                         </div>
 
                         <button className="primary-button" type="submit">
