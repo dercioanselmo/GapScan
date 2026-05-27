860afc69d5d7def0be5357b3ef92cbbacdf8913e
Dercio Anselmo <dercio@branchdev.io>
2026-05-27 14:19:36 +0400

    Auth logic implementation

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import {usePuterStore} from "~/lib/puter";
+
+
+export const meta = () =>([
+    {title: 'GapScan | Auth'},
+    {name: 'description', content: 'Log into your account'},
+])
 
 const Auth = () => {
+    const { isLoading, auth } = usePuterStore();
     return (
-        <div>Auth</div>
+        <main className="bg-[url('images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+            <div className="gradient-border shadow-lg">
+                <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+                    <div className="flex flex-col items-center gap-2 text-center">
+                        <h1>Welcome</h1>
+                        <h2>Log In to continue your job jorney</h2>
+                    </div>
+                    <div>
+                        {isLoading ? (
+                            <button className="auth-button animate-pulse">
+                                <p>Signing you in . . .</p>
+                            </button>
+                        ) : (
+                            <>
+                                {auth.isAuthenticated ? (
+                                    <button className="auth-button" onClick={auth.signOut}>
+                                    <p>Log Out</p>
+                                    </button>
+                                ) : (
+                                    <button className="auth-button" onClick={auth.signIn}>
+                                        <p>Log in</p>
+                                    </button>
+                                )}
+                            </>
+                        )}
+                    </div>
+                </section>
+            </div>
+        </main>
     )
 }
 export default Auth
