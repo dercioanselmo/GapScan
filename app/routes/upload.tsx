b9c3dfbef947b40e80e642fc43e1bc19ad17bea8
Dercio Anselmo <dercio@branchdev.io>
2026-05-27 23:16:29 +0400

    Resume Analysis

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -41,7 +41,20 @@ const Upload = () => {
         }
         await kv.set(`resume:${ uuid }`, JSON.stringify(data));
         setStatusText("Analyzing . . .")
+        const feedback = await ai.feedback(
+            uploadFile.path,
+            prepareInstructions({jobTitle, jobDescription})
+        )
+        if(!feedback) return setStatusText("Error: Failed to analyze resume");
 
+        const feedbackText = typeof feedback.message.content === 'string'
+        ? feedback.message.content
+            : feedback.message.content[0].text;
+
+        data.feedback = JSON.parse(feedbackText);
+        await kv.set(`resume:${ uuid }`, JSON.stringify(data));
+        setStatusText("Analysis complete, redirecting . . .");
+        console.log(data);
     }
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
