6f1f5b3e9cc3493cfb63825c85215d6a4a4ef1a1
Dercio Anselmo <dercio@branchdev.io>
2026-05-28 15:29:09 +0400

    Score Gauge

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import ScoreGauge from "~/components/ScoreGauge";
 
-const Summary = () => {
+const Summary = ({ feedback }: { feedback: Feedback}) => {
     return (
-        <div>Summary</div>
+        <div className="bg-white rounded-2xl shadow-md w-full">
+            <div className="flex flex-row items-center p-4 gap-8">
+                <ScoreGauge score={feedback.overallScore} />
+
+            </div>
+        </div>
     )
 }
 export default Summary
