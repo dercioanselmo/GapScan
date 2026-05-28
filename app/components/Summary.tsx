92ffda77a6801a46e95cbb8979548a42dc70cdb5
Dercio Anselmo <dercio@branchdev.io>
2026-05-28 15:57:03 +0400

    Summary - UI - Score

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,14 +1,41 @@
 import React from 'react'
 import ScoreGauge from "~/components/ScoreGauge";
 
-const Summary = ({ feedback }: { feedback: Feedback}) => {
+const Category = ({ title, score }: { title: string, score: number }) => {
+    const textColor = score > 70 ? 'text-green-600'
+        : score > 49
+    ? 'text-yellow-600' : 'text-red-600'
     return (
-        <div className="bg-white rounded-2xl shadow-md w-full">
-            <div className="flex flex-row items-center p-4 gap-8">
-                <ScoreGauge score={feedback.overallScore} />
-
+        <div className="resume-summary">
+            <div className="category">
+                <p>{title}</p>
             </div>
+            <p className="text-2xl">
+                <span className={textColor}></span>/100
+            </p>
         </div>
     )
 }
+
+const Summary = ({ feedback }: { feedback: Feedback }) => {
+    return (
+        <div className="bg-white rounded-2xl shadow-md w-full">
+            <div className="flex flex-row items-center p-4 gap-8">
+                <ScoreGauge score={feedback.overallScore ?? 0} />
+                <div className="flex flex-col gap-2">
+                    <h2 className="text-2xl font-bold">Your Resume Score</h2>
+                    <p className="text-sm text-gray-500">
+                        This score is calculated based on the variables listed below.
+                    </p>
+                </div>
+            </div>
+
+            <Category title="Tone & Style" score={feedback.toneAndStyle?.score ?? 0} />
+            <Category title="Content" score={feedback.content?.score ?? 0} />
+            <Category title="Structure" score={feedback.structure?.score ?? 0} />
+            <Category title="Skills" score={feedback.skills?.score ?? 0} />
+        </div>
+    )
+}
+
 export default Summary
